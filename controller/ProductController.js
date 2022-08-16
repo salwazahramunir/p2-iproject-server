cc5dd535c15e66d2151b756143ec25aca1dd553e
salwazahramunir <salwazahramunir@gmail.com>
2022-08-16 23:11:14 +0700

    update product done

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -8,7 +8,7 @@ class ProductController {
                 attributes: { exclude: ["createdAt", "updatedAt"] },
                 include: [{
                     model: Brand,
-                    attributes: { exclude: ["createdAt", "updatedAt", "publicIdLogo"] },
+                    attributes: { exclude: ["createdAt", "updatedAt", "publicIdImage"] },
                 }]
             })
 
@@ -39,6 +39,61 @@ class ProductController {
                 data: newProduct
             })
         } catch (error) {
+            next(error)
+        }
+    }
+
+    static async updateProduct(req, res, next) {
+        try {
+            const { productId } = req.params
+
+            let findProduct = await Product.findByPk(+productId)
+
+            if (!findProduct) {
+                throw { name: "NotFound" }    
+            }
+
+            let file = req.files
+            const { nameProduct, price, weight, skinCategory, productCategory, description, BrandId } = req.body
+            let dataCloudinary = {}
+
+            if (!file) {
+                file = {
+                    tempFilePath: findProduct.imageProduct
+                }
+            } else {
+                file = req.files.imageProduct
+                dataCloudinary = await uploadToCloudinary(file.tempFilePath, 'p2-individual-project/image-product')
+            }
+
+            if (!nameProduct || !price || !weight || !skinCategory || !productCategory || !description || !BrandId ) {
+                await deleteToCloudinary(dataCloudinary.public_id, 'p2-individual-project/image-product')
+            }
+
+            await Product.update({ nameProduct, imageProduct: dataCloudinary.secure_url, publicIdImage: dataCloudinary.public_id, price, weight, skinCategory, productCategory, description, BrandId }, { where: { id: productId }})
+
+            if (file.name) {
+                await deleteToCloudinary(findProduct.publicIdImage, 'p2-individual-project/image-product')
+            }
+
+            findProduct = await Product.findByPk(+productId)
+            
+            res.status(200).json({
+                message: `Success updated ${findProduct.nameProduct}`,
+                data: {
+                    id: findProduct.id,
+                    nameProduct: findProduct.nameProduct,
+                    imageProduct: findProduct.imageProduct,
+                    price: findProduct.price,
+                    weight: findProduct.weight,
+                    skinCategory: findProduct.skinCategory,
+                    productCategory: findProduct.productCategory,
+                    description: findProduct.description,
+                    BrandId: findProduct.BrandId,
+                }
+            })
+        } catch (error) {
+            console.log(error);
             next(error)
         }
     }
--- router/productRouter.js
+++ router/productRouter.js
@@ -3,5 +3,6 @@ const ProductController = require('../controller/ProductController')
 
 productRouter.get('/', ProductController.readAllProduct)
 productRouter.post('/', ProductController.addProduct)
+productRouter.put('/:productId', ProductController.updateProduct)
 
 module.exports = productRouter
